cc7fbae3e335bebda359ce442bb279b2d6974247
László Kiss <kisslac1988@hotmail.com>
2020-01-07 21:57:31 +0100

    Add IoT sensor configuration route

--- src/router/device-configuration.router.ts
+++ src/router/device-configuration.router.ts
@@ -4,6 +4,6 @@ import { deviceConfigurationHandler } from '../handlers/device-configuration.han
 export const deviceConfigurationRouter = Router();
 
 deviceConfigurationRouter
-    .get("/:deviceId/:sensorIndex/configuration", deviceConfigurationHandler.getIotDeviceConfiguration)
+    .get("/:deviceId/:sensorIndex/iot/configuration", deviceConfigurationHandler.getIotDeviceConfiguration)
     .get("/:deviceId/configuration", deviceConfigurationHandler.getDeviceConfiguration)
     .put("/:deviceId/configuration", deviceConfigurationHandler.updateDeviceConfiguration)
